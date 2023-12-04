34aee77a6a650c601b7facfe9db2436d028a11f9
beebekisme <021a306@sxc.edu.np>
2023-12-04 18:34:31 +0545

    rem target

--- Day1/target/debug/Day1.d
+++
@@ -1,1 +0,0 @@
-D:\CODE\Rust_Projects\Advent_Of_Code\Day1\target\debug\Day1.exe: D:\CODE\Rust_Projects\Advent_Of_Code\Day1\src\main.rs
--- Day1/target/debug/deps/Day1-5593433fe0a8b297.d
+++
@@ -1,5 +0,0 @@
-d:\CODE\Rust_Projects\Advent_Of_Code\Day1\target\debug\deps\Day1-5593433fe0a8b297.rmeta: src\main.rs
-
-d:\CODE\Rust_Projects\Advent_Of_Code\Day1\target\debug\deps\Day1-5593433fe0a8b297.d: src\main.rs
-
-src\main.rs:
